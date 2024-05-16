2d9d3041302e405e1a8b7779be6bad7cc3187fd8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 13:48:58 +0530

    Update Footer.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Footer.jsx
+++ client/src/comportments/HomePage/Footer.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 
 const Footer = () => {
     const styles = {
-        background: 'linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)), url("https://img.freepik.com/premium-photo/amazing-homemade-pizza-black-stone-background_952778-18218.jpg?size=626&ext=jpg&ga=GA1.2.1865485918.1713421678&semt=ais_user")',
+        background: 'linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)), url("https://img.freepik.com/premium-photo/amazing-homemade-pizza-black-stone-background_952778-18218.jpg?size=626&ext=jpg&ga=GA1.2.1865485918.1713421678&semt=ais_user")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }
     // 
   return (
     <div>
-        <div className='py-14 lg:px-16 px-0 h-full lg:h-auto h-auto ' style={styles}>
+        <div className='py-14 lg:px-16 px-0 h-full lg:h-[80vh] h-auto text-white' style={styles}>
+            <div className="lg:grid grid-cols-2 gap-4">
+                <div className=""></div>
+                <div className="">
+                    <div className="">
+                        <div className="lg:grid grid-cols-2 gap-2">
+                            <div className="">
+                                <h1 className="">The Cafe</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
