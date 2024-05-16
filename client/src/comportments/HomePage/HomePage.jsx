e42d859b6295abe2fab460fbf09d865c13d55a8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 13:16:42 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   const styles = {
-    background: 'linear-gradient(to right, white, rgba(0,0,0,0.9)), url("https://wallpapercave.com/wp/wp12420121.jpg")',
+    background: 'linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)), url("https://wallpapercave.com/wp/wp12420121.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   }
@@ -10,7 +10,7 @@ const HomePage = () => {
     <div>
         <div className='py-14 lg:px-16 px-0 h-full lg:h-auto h-auto ' style={styles}>
           <div className="bg-none text-white mt-[-20px]">
-            Hi all
+           
           </div>
         </div>
     </div>
