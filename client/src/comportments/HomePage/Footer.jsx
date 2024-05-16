1fe6012e2e64a8011af38d71a62ce9b39a94502e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 13:45:08 +0530

    Create Footer.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/HomePage/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    const styles = {
+        background: 'linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)), url("https://img.freepik.com/premium-photo/amazing-homemade-pizza-black-stone-background_952778-18218.jpg?size=626&ext=jpg&ga=GA1.2.1865485918.1713421678&semt=ais_user")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }
+    // 
+  return (
+    <div>
+        <div className='py-14 lg:px-16 px-0 h-full lg:h-auto h-auto ' style={styles}>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
