809bb4137a8dd093507ba2df765a4a301591e738
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 10:38:32 +0530

    Update Discount.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Discount.jsx
+++ client/src/comportments/HomePage/Discount.jsx
@@ -10,8 +10,10 @@ const Discount = () => {
             <div className="my-8">
               <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
               </div>
-              <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-              <p className="">Get 10% OFF for Evey Product on Friday</p>
+            <div className="">
+                <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+                <p className="">Get 10% OFF for Evey Product on Friday</p>
+            </div>
             </div>
             <div className="my-8">
               <div className="bg-[url(https://wallpapercave.com/wp/wp12452172.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
