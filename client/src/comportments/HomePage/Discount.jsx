a7f45aba8685fe3ffb6f1a2386dcc11e216ac8f8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 10:48:55 +0530

    Update Discount.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Discount.jsx
+++ client/src/comportments/HomePage/Discount.jsx
@@ -6,50 +6,39 @@ const Discount = () => {
     <div>
         <div className="my-24 mx-12">
           <h1 className="text-center text-4xl rest-title text-gray-500">Discounts</h1>
-          <div className="lg:grid grid-cols-4 gap-4 my-64">
-            <div>
-              <div className="h-64">
-                <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
+          <div className="lg:grid grid-cols-4 gap-4 mb-40">
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
+                <div className="">
+                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+                    <p className="">Get 10% OFF for Evey Product on Friday</p>
                 </div>
+              </div>
+              
               <div className="">
-                  <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                  <p className="">Get 10% OFF for Evey Product on Friday</p>
+                <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
+                <div className="">
+                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                </div>
               </div>
-              </div>
-            </div>
 
-            <div>
-              <div className="h-64">
-                <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
-                </div>
               <div className="">
-                  <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                  <p className="">Get 10% OFF for Evey Product on Friday</p>
+                <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
+                <div className="">
+                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                </div>
               </div>
-              </div>
-            </div>
 
-            <div>
-              <div className="h-64">
-                <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
-                </div>
               <div className="">
-                  <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                  <p className="">Get 10% OFF for Evey Product on Friday</p>
+                <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" className='w-full'/>
+                <div className="">
+                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                </div>
               </div>
-              </div>
-            </div>
 
-            <div>
-              <div className="h-64">
-                <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
-                </div>
-              <div className="">
-                  <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                  <p className="">Get 10% OFF for Evey Product on Friday</p>
-              </div>
-              </div>
-            </div>
 
           </div>
         </div>
