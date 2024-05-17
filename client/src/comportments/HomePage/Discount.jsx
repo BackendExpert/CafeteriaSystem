4dc03b2bc43f5636c2b0002d7ee922656085cec8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 10:54:47 +0530

    Update Discount.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Discount.jsx
+++ client/src/comportments/HomePage/Discount.jsx
@@ -6,7 +6,7 @@ const Discount = () => {
     <div>
         <div className="my-24 mx-12">
           <h1 className="text-center text-4xl rest-title text-gray-500">Discounts</h1>
-          <div className="lg:grid grid-cols-4 gap-4 my-4">
+          <div className="lg:grid grid-cols-4 gap-6 my-16">
             <div className="">
                 <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
                 <div className="text-center">
@@ -18,24 +18,24 @@ const Discount = () => {
               <div className="">
                 <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
                 <div className="text-center">
-                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                    <h1 className="text-3xl rest-title py-4">Pizza Day</h1>
+                    <p className="">Get 50% OFF for Selected Pizza</p>
                 </div>
               </div>
 
               <div className="">
                 <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt="" />
                 <div className="text-center">
-                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                    <h1 className="text-3xl rest-title py-4">Bugur Wednesday</h1>
+                    <p className="">Get up to 25% OFF for Bugurs on Wednesdays</p>
                 </div>
               </div>
 
               <div className="">
                 <img src="https://wallpapercave.com/wp/wp11905779.jpg" alt=""/>
                 <div className="text-center">
-                    <h1 className="text-3xl rest-title py-4">Black Friday</h1>
-                    <p className="">Get 10% OFF for Evey Product on Friday</p>
+                    <h1 className="text-3xl rest-title py-4">Black Coffie Moday</h1>
+                    <p className="">Get up to 40% OFF for Coffies on Every Mondays</p>
                 </div>
               </div>
 
