ef86ac8a3fb9945bf7a0772c5fcca063fdce0b76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 10:35:06 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -34,17 +34,17 @@ const HomePage = () => {
         </div>
 
 
-        <div className="my-16 mx-12">
+        <div className="my-24 mx-12">
           <h1 className="text-center text-4xl rest-title text-gray-500">Discounts</h1>
-          <div className="lg:grid grid-cols-4 gap-4 my-8">
-            <div className="">
-              <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl">
-                <h1 className="text-2xl rest-title text-white text-center py-16">Black Friday</h1>
+          <div className="lg:grid grid-cols-4 gap-4 my-16">
+            <div className="my-8">
+              <div className="bg-[url(https://wallpapercave.com/wp/wp11905779.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
               </div>
+              <h1 className="text-3xl rest-title py-4">Black Friday</h1>
+              <p className="">Get 10% OFF for Evey Product on Friday</p>
             </div>
-            <div className="">
-              <div className="bg-[url(https://wallpapercave.com/wp/wp12452172.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl">
-                <h1 className="text-2xl rest-title text-white text-center py-16">Black Friday</h1>
+            <div className="my-8">
+              <div className="bg-[url(https://wallpapercave.com/wp/wp12452172.jpg)] bg-cover bg-center h-full duration-500 hover:text-3xl rounded">
               </div>
             </div>
             <div className="">
@@ -55,7 +55,11 @@ const HomePage = () => {
             </div>
           </div>
         </div>
+
+        <div className="my-16">
         <Footer />
+        </div>
+
     </div>
   )
 }
