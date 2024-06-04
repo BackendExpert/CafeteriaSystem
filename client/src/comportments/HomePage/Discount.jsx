b8f6e3c334a6254f3741aa40a95c66fc27c90126
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 12:43:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Discount.jsx
+++ client/src/comportments/HomePage/Discount.jsx
@@ -4,56 +4,7 @@ import MyIcons from '@reacticons/ionicons'
 const Discount = () => {
   return (
     <div>
-        <div className="my-24 lg:mx-12">
-          <h1 className="text-center text-4xl rest-title text-gray-500">Discounts</h1>
-          <div className="mx-16">    
-            <div className="lg:grid grid-cols-2 gap-4">
-            <div className="lg:flex my-12">
-              <img src="https://wallpapercave.com/wp/wp1929360.jpg" alt="" className='h-40 w-auto rounded-xl'/>
-              <div className="ml-6 my-4">
-                <h1 className="rest-title text-gray-500 text-3xl">Black Friday</h1>
-                <p className="my-2">
-                  Get up to 30% OFF on Every Friday From 4.00pm to 7.30pm
-                </p>
-              </div>
-            </div>            
-
-            <div className="lg:flex my-12">
-              <img src="https://wallpapercave.com/wp/wp12451912.jpg" alt="" className='h-40 w-auto rounded-xl'/>
-              <div className="ml-6 my-4">
-                <h1 className="rest-title text-gray-500 text-3xl">Pizza Day</h1>
-                <p className="my-2">
-                  Get up to 50% OFF on Selected Pizza
-                </p>
-              </div>
-            </div>
-
-
-            <div className="lg:flex my-12">
-              <img src="https://wallpapercave.com/wp/wp11905761.jpg" alt="" className='h-40 w-auto rounded-xl'/>
-              <div className="ml-6 my-4">
-                <h1 className="rest-title text-gray-500 text-3xl">Bugur TuesDay</h1>
-                <p className="my-2">
-                    Get up to 75% OFF on Bugurs on Tuesdays
-                </p>
-              </div>
-            </div>
-
-            <div className="lg:flex my-12">
-              <img src="https://wallpapercave.com/uwp/uwp4331656.jpeg" alt="" className='h-40 w-auto rounded-xl'/>
-              <div className="ml-6 my-4">
-                <h1 className="rest-title text-gray-500 text-3xl">Coffee Monday</h1>
-                <p className="my-2">
-                    Get up to 50% OFF on Coffee on Mondays
-                </p>
-              </div>
-            </div>
-
-            </div>        
-
-
-
-          </div>
+        <div className="my-24 md:mx-12 bg-[]">
 
         </div>
     </div>
