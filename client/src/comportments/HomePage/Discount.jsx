661fab1047d17f3025bcf0718363dc8512529b84
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 13:02:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Discount.jsx
+++ client/src/comportments/HomePage/Discount.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import MyIcons from '@reacticons/ionicons'
+import bgCafe from '../../assets/bgcafe.jpg'
 
 const Discount = () => {
   const styles = {
-    background: 'linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.3)), url("https://img.freepik.com/free-photo/top-view-delicious-muffins-with-nuts-copy-space_23-2148833985.jpg?size=626&ext=jpg&ga=GA1.1.1865485918.1713421678&semt=ais_user")',
+    background: `linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.3)), url("${bgCafe}")`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   }
   return (
     <div>
-        <div className="py-24 md:px-12" style={styles}>
+        <div className="py-24 md:px-12">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam dolores doloremque, dolorem consequuntur voluptatibus nisi repellendus laboriosam, libero, eaque quis ea modi voluptatum. Itaque magnam veritatis neque recusandae et ut?
         </div>
     </div>
