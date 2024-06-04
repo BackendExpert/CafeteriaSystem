8c72ed548b2d36cf061665d3dc91733dca16bc85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 13:13:50 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -19,17 +19,17 @@ const HomePage = () => {
   }
   return (
     <div>
-        <div className='py-14 lg:px-16 px-0 h-full lg:h-[80vh] h-auto ' style={styles}>
-          <div className="bg-none text-white lg:ml-20 ml-0">
-            <div className="lg:grid grid-cols-2 gap-4">
-              <div className="lg:mx-0 mx-4 my-8">
+        <div className='py-14 md:px-16 px-0 h-[80vh]' style={styles}>
+          <div className="bg-none text-white md:ml-20 ml-0">
+            <div className="md:grid grid-cols-2 gap-4">
+              <div className="md:mx-0 mx-4 my-8">
                 <p className="">Welcome to</p>
                 <h1 className="text-5xl tracking-[0.3em] rest-title">The Best Cafe</h1>
                 <p className="my-4">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio consequatur accusantium modi molestiae recusandae qui harum animi maxime hic! Facilis repudiandae deleniti nobis modi illum ducimus sit corrupti at.
                 </p>
 
-                <div className="lg:flex">
+                <div className="md:flex">
                   <button className="bg-yellow-500 py-4 px-8 rounded duration-500">Be A Member</button>
                   <button className="mx-4 border border-yellow-500 rounded py-4 px-8 rounded duration-500 hover:bg-yellow-500">SignIn</button>                  
                 </div>
